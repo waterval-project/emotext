e3ce168152fa5df6e472461f18475aa0f7a4f297
waterval-project <65618661+waterval-project@users.noreply.github.com>
2020-05-21 21:37:35 +0200

    Update rainbowmaster.js

--- rainbowmaster.js
+++ rainbowmaster.js
@@ -620,7 +620,7 @@ function showw() {
     if(ccc>99){ccc=0}
   }
   //$('#lista').html(cad+'<br>'+fr)
-  $('#accion').html(cad + '<br><a href="https://waterval-project.github.io/emotext/" target="_blank" >You can send me emails with rainbows from here</a>')
+  $('#accion').html(cad + '<br><a href="https://waterval-project.github.io/emotext/" target="_blank" style="font-size:-2;" >You can send me emails with rainbows from here</a>')
   //alert(cad)
 
 }
